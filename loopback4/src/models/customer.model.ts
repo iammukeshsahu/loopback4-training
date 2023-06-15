f6969bcc40a10d64e36e8f7928bc5e023411a549
Mukesh Sahu <mukesh.sahu@pentair.com>
2023-06-15 11:22:13 +0530

    loopback4-exercise4

---
+++ loopback4/src/models/customer.model.ts
@@ -0,0 +1,42 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import { Users } from './users.model';
+
+@model()
+export class Customer extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  address?: string;
+
+  @property({
+    type: 'string',
+  })
+  website?: string;
+
+  @hasMany(() => Users)
+  users: Users[];
+
+  constructor(data?: Partial<Customer>) {
+    super(data);
+  }
+}
+
+export interface CustomerRelations {
+  // describe navigational properties here
+}
+
+export type CustomerWithRelations = Customer & CustomerRelations;
