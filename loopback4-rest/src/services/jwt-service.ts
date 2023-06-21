be1ce7465a293e23e62835d9466170e6ca16cf39
Mukesh Sahu <mukesh.sahu@pentair.com>
2023-06-21 11:40:36 +0530

    used env variable

--- loopback4-rest/src/services/jwt-service.ts
+++ loopback4-rest/src/services/jwt-service.ts
@@ -7,7 +7,7 @@ const secretKey: string | undefined = process.env.SECRET_KEY || '';
 
 export class TokenService {
     createJwtToken(userData: UserProfile): string {
-        const token = jwt.sign({ userData }, 'secretKey', { expiresIn: '1h' });
+        const token = jwt.sign({ userData }, secretKey , { expiresIn: expiryTime});
         return token;
       }
 }
