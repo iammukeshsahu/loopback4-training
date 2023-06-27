30216fb9dcdc768be9a744ce92045de84761c186
Mukesh Sahu <mukesh.sahu@pentair.com>
2023-06-27 08:43:59 +0530

    reverted changes

--- sourceloop/packages/migrations/authetication-service/migrations/sqls/20201220124353-add-todo-perms-down.sql
+++
@@ -1,1 +0,0 @@
-update main.roles set permissions=array_remove(permissions,'TodoCreator') where role_type IN (0,2);
--- sourceloop/packages/migrations/authetication-service/migrations/sqls/20201220124353-add-todo-perms-up.sql
+++
@@ -1,2 +0,0 @@
-UPDATE main.roles SET permissions = permissions || '{TodoCreator}'
-where role_type in (0,2);
